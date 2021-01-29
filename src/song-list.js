d3c54d50fd54beeabbbe3cb7da41d7258a05af1b
jerryfangr <2a15068517423@gmail.com>
2021-01-29 15:18:37 +0800

    add songlist fetch and render to page

--- src/song-list.js
+++ src/song-list.js
@@ -1,4 +1,5 @@
 import eventHub from './event-hub';
+import { AV } from "./av";
 
 let view = {
   el: '.page > aside > #songsContainer',
@@ -16,13 +17,13 @@ let view = {
     if (data === undefined || data.songs.length === 0) {
       return this.dom.innerHTML = this.template;
     }
-
-    let songContainer = this.dom.querySelector('.song-list');
+    
+    this.songContainerDom = this.songContainerDom || this.dom.querySelector('.song-list');
     let html = ""
     data.songs && data.songs.forEach(song => {
       html += "<li>" + song.name +"</li>";
     });
-    songContainer.innerHTML = html;
+    this.songContainerDom.innerHTML = html;
   },
   deActive () {
     let activeDom = this.find('.active');
@@ -34,6 +35,18 @@ view.init();
 let model = {
   data: {
     songs: []
+  },
+  find () {
+    var query = new AV.Query('Song');
+    return query.find()
+      .then(songs => {
+        this.data.songs = songs.map((song => {
+          let newSong = { ...song.attributes}
+          newSong.id = song.id;
+          return newSong;
+        }))
+        return this.data.songs;
+      });
   }
 }
 
@@ -42,11 +55,25 @@ let controller = {
     this.view = view;
     this.model = model;
     this.view.render(this.model.data);
+    this.bindEvents();
+    this.bindEventHub();
+    this.fetSongs()
+  },
+  fetSongs () {
+    this.model.find()
+      .then(() => {
+        this.view.render(this.model.data)
+      })
+  },
+  bindEvents () {
+
+  },
+  bindEventHub () {
     eventHub.on('upload', data => {
       this.view.deActive()
     })
     eventHub.on('create', data => {
-      // 这里如果data是对象，且不能保证传过来的是新的，就要对data clone
+      // 这里如果data是对象，且不能保证传过来的是新的，就要创建新对象
       // 以防一直存同一个对象的引用，或者被对面改了数据
       this.model.data.songs.push(data);
       this.view.render(this.model.data)
